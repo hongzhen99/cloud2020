bed6456f95a36ec4dfc1f6618865304ff0d48b78
tianhongzhen <2547620130@qq.com>
2020-07-16 17:36:28 +0800

    "config 全局配置"

---
+++ cloud-config-client3355/src/main/java/com/tian/ConfigClientMain3355.java
@@ -0,0 +1,19 @@
+package com.tian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigClientMain3355
+ * @Description TODO
+ * @Author tianhongzhen
+ * @Date: 2020/7/16 上午10:25
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class,args);
+    }
+}
---
+++ cloud-config-client3355/src/main/java/com/tian/controller/ConfigClientController.java
@@ -0,0 +1,24 @@
+package com.tian.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName ConfigClientController
+ * @Description TODO
+ * @Author tianhongzhen
+ * @Date: 2020/7/16 上午10:26
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String aa(){
+        return configInfo;
+    }
+}
