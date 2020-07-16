b5fcceb65b998f5dd259df4abb933a29a83b00e1
tianhongzhen <2547620130@qq.com>
2020-07-16 18:30:59 +0800

    "Bus 消息总线"

---
+++ cloud-config-client3366/src/main/java/com/tian/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package com.tian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @ClassName ConfigClientMain3366
+ * @Description TODO
+ * @Author tianhongzhen
+ * @Date: 2020/7/16 下午5:51
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
